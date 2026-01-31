f87cf4a4bed46d270eabd1ea23e335ad4bf62c67
freemoney-11 <himansd185@gmail.com>
2026-02-01 02:10:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,28 @@
-let welcomePlayed = false;
-const bypassAudio = document.getElementById('bypassAudio');
 const newUserAudio = document.getElementById('newUserAudio');
 const depositAudio = document.getElementById('depositAudio');
 
-// 1. Page pe pehla touch hote hi Welcome Audio
-function playWelcome() {
+let welcomePlayed = false;
+
+// 1. Welcome Audio (Pehle touch par)
+function playWelcomeOnce() {
     if (!welcomePlayed) {
-        newUserAudio.play();
+        newUserAudio.play().catch(e => console.log("Touch to start"));
         welcomePlayed = true;
     }
 }
 
-// 2. Login Button Logic
-function handleLogin() {
-    bypassAudio.play();
-    setTimeout(() => {
-        // Login dabane par wapas register pe bhej raha hai
-        window.location.href = "https://www.dmwin1.com/#/register?invitationCode=27278184971";
-    }, 1500);
-}
-
-// 3. Batch/Deposit Logic
-function handleBatch() {
+// 2. Register click -> Deposit Audio -> 10s Wait -> Final Link
+function handleRegisterAndDeposit() {
+    // Deposit wala audio play karo
     depositAudio.play();
-    // Naya tab me deposit khulega
-    window.open("https://www.dmwin1.com/#/wallet/Recharge", "_blank");
     
-    // 8 second baad final batch link
+    // Website ko Deposit page par le jao
+    setTimeout(() => {
+        window.location.href = "https://www.dmwin1.com/#/wallet/Recharge";
+    }, 1000);
+
+    // 10 second ka zabardasti wait (Forceful redirection)
     setTimeout(() => {
         window.location.href = "https://freemoney-11.github.io/Gitbu/";
-    }, 8000);
+    }, 11000); // 1s loading + 10s wait
 }
